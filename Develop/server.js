69d9104b01825bc5e75890d2a75bf10629c7b854
vsteere <vssteere@gmail.com>
2020-09-17 20:07:17 -0500

    API routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 //dependencies
 let express = require("express");
 let fs = require("fs");
+let db = require("./db/db.json")
 
 //Express Server
 let work = express();
@@ -15,29 +16,17 @@ work.use(express.json());
 //HTML routes
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, ".notes.html"));
 
 app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "index.html"));
 
 //API routes
+//this gets the db.json file and returns the saved notes as JSON
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/api/notes", function(req, res) {
+    res.json(db);
+  });
 
 
 
