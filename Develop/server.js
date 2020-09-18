92117c3a7eea4dda44063cd583fdf10c8a33f493
vsteere <vssteere@gmail.com>
2020-09-17 20:22:22 -0500

    update let

--- Develop/server.js
+++ Develop/server.js
@@ -5,30 +5,34 @@ let db = require("./db/db.json");
 let path = require("path");
 
 //Express Server
-let work = express();
+let app = express();
 
 let PORT = 3000;
 
 //sets up Express to handle data
 
-work.use(express.urlencoded({ extended: true }));
-work.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //HTML routes
 
-work.get("/notes", function(req, res) {
+app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
-work.get("*", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 // //API routes
 // //this gets the db.json file and returns the saved notes as JSON
-work.get("/api/notes", function(req, res) {
+app.get("/api/notes", function(req, res) {
     res.json(db);
   });
+//pulls the note and posts it in teh db.json file
+ app.post("/api/notes", function(req, res) {
+    db.push(req.body);
+      });
 
 
 
